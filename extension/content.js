86569c68f585d9dd305be8a667278d4a118af7a7
Guilherme Casagrande <thegui4000@gmail.com>
2023-11-21 14:18:29 -0300

    feat: implemantado contagem do instagram

--- extension/content.js
+++ extension/content.js
@@ -41,8 +41,11 @@ const escutador = (url) => {
 
         //excluir.forEach(element => element.addEventListener('click', exclui))
     } else if (url == "www.instagram.com") {
-        if (window.location.href.split("/")[3] == "direct" && window.location.href.split("/")[4] == "t") {
-            console.log(123)
+        if (
+            window.location.href.split("/")[3] == "direct" &&
+            window.location.href.split("/")[4] == "t"
+        ) {
+            // console.log(123)
             let chatI = document.querySelector(
                 ".x6s0dn4.x78zum5.x1gg8mnh.x1pi30zi.xlu9dua"
             );
@@ -55,12 +58,20 @@ const escutador = (url) => {
             cliqueI.forEach((element) => {
                 element.addEventListener("click", contaClique);
             });
-        }else{
-            let chatI = document.querySelectorAll('.x1i0vuye.xvbhtw8.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x5n08af.x78zum5.x1iyjqo2.x1qlqyl8.x1d6elog.xlk1fp6.x1a2a7pz.xexx8yu.x4uap5.x18d9i69.xkhd6sd.xtt52l0.xnalus7.xs3hnx8.x1bq4at4.xaqnwrm')
-            chatI.forEach(element => element.addEventListener('keyup', contaEnterC))
+        } else {
+            let chatI = document.querySelectorAll(
+                ".x1i0vuye.xvbhtw8.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x5n08af.x78zum5.x1iyjqo2.x1qlqyl8.x1d6elog.xlk1fp6.x1a2a7pz.xexx8yu.x4uap5.x18d9i69.xkhd6sd.xtt52l0.xnalus7.xs3hnx8.x1bq4at4.xaqnwrm"
+            );
+            chatI.forEach((element) =>
+                element.addEventListener("keydown", contaEnterComenInsta)
+            );
 
-            cliqueI = document.querySelectorAll('.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37')
-            cliqueI.forEach(element => element.style.background = 'red')
+            cliqueI = document.querySelectorAll(
+                ".x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.x173jzuc.x1yc6y37"
+            );
+            cliqueI.forEach((element) => {
+                element.addEventListener("click", contaCliqueComenInsta);
+            });
         }
     }
 };
@@ -87,7 +98,7 @@ function contaEnter(event) {
 }
 
 function contaEnterC(event) {
-    if (event.which === 13 && ultimaC !== "") {
+    if (event.which === 13 && ultimaC != "") {
         setTimeout(() => {
             if (event.srcElement.children[0].children[0].textContent == "") {
                 window.location.href.split("/")[2] == "www.facebook.com"
@@ -96,10 +107,31 @@ function contaEnterC(event) {
             }
         }, 1000);
     }
+    ultimaC = event.srcElement.children[0].children[0].textContent;
 }
 
 function exclui(event) {
     salvarMsgs("excluidas");
+}
+
+function contaCliqueComenInsta(event) {
+    if (event.srcElement.textContent == "Publicar") {
+        salvarMsgs("comentarioInstagram");
+    }
+}
+function contaEnterComenInsta(event) {
+    // console.log(event.srcElement.textContent);
+    if (event.which === 13 && event.srcElement.textContent != "") {
+        setTimeout(() => {
+            // console.log(event.srcElement.textContent);
+            if (event.srcElement.textContent == "") {
+                window.location.href.split("/")[2] == "www.facebook.com"
+                    ? salvarMsgs("comentarioFacebook")
+                    : salvarMsgs("comentarioInstagram");
+            }
+        }, 2000);
+    }
+    ultimaC = event.srcElement.textContent;
 }
 
 function salvarMsgs(type) {
